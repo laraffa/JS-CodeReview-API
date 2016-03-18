3c3f4253318e569ed0ae47eaa6323a41851f7f43
lindsey <lindsey.raffa@gmail.com>
2016-03-18 10:42:26 -0700

    add user info

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,9 +2,15 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
 
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+  $('#githubInfo').click(function() {
+    var github = $('#userName').val();
+    $('#userName').val("");
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      // $('.showUserInfo').text("This github user is " + github + " " + response.name + " is " + response.avatar_url + response.location);
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+      $('.showUserInfo').text(error.responseJSON.message);
+    });
   });
-};
+});
